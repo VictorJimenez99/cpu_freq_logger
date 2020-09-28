0fc719fa3b7692b372833667d60a40008441ba5d
VictorJimenez99 <jimenezvictor99@gmail.com>
2020-09-28 13:09:15 -0500

    Better Output

--- script.R
+++ script.R
@@ -1,8 +1,23 @@
-library(tidyverse)
+library(ggplot2)
+library(tibble)
+library(dplyr)
+library(readr)
+library(patchwork)
 #Data in ----
 ds<-read_csv("freq_log.csv")
 #plotting ----
-ggplot(ds) + geom_line(aes(x=log_id, y=freq, col = cpu_id))+ facet_wrap(~cpu_id)+
+general<-ggplot(ds) + geom_line(aes(x=sample_id, y=freq, col = cpu_id))+ facet_wrap(~cpu_id)+
   theme(legend.position = "none") + xlab("Miliseconds") + ylab("Frequency") + 
-  scale_y_continuous(limits = c(0,2))
+  scale_y_continuous()
+
+
+samples <- max(ds$sample_id);
+
+sub_sec <- ds %>% filter(sample_id > samples*.9 & samples)
+sub_sec <- ggplot(sub_sec) + geom_line(aes(x=sample_id, y=freq, col = cpu_id))+ facet_wrap(~cpu_id)+
+  theme(legend.position = "none") + xlab("Miliseconds") + ylab("Frequency") + 
+  scale_y_continuous()
+
+final<-general/sub_sec 
+final & theme(legend.position = "none")
 
