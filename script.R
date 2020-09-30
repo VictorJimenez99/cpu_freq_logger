511963930466e8b88f2e32748046af91c90093d4
VictorJimenez99 <jimenezvictor99@gmail.com>
2020-09-30 17:33:07 -0500

    added new plot

--- script.R
+++ script.R
@@ -6,11 +6,15 @@ library(patchwork)
 #detach("package:dplyr", unload = T)
 
 #Data in ----
-col_esp <-
+# Specify data_in
+col_type <-
   cols(cpu_id = col_character(),
        freq = col_double(),
-       sample_id = col_double())
-ds <- read_csv("freq_log.csv" , col_types = col_esp)
+       sample_id = col_double(),
+       time_passed = col_factor())
+
+ds <- read_csv("freq_log.csv" , col_types = col_type)
+rm("col_type")
 #plotting ----
 general <- ggplot(ds) +
   geom_line(aes(x = sample_id, y = freq, col = cpu_id)) +
@@ -19,7 +23,7 @@ general <- ggplot(ds) +
   scale_y_continuous() + ggtitle("Benchmark") +
   theme(legend.position = "none")
 
-ggsave("Benchmark.png",
+ggsave("benchmark.png",
        plot = general,
        width = 30,
        height = 5)
@@ -50,3 +54,31 @@ ggsave("result.png",
        plot = final,
        width = 7,
        height = 7)
+rm("final","general", "sub_sec", "samples")
+
+#data----
+
+
+grouped<-ds %>% group_by(cpu_id,time_passed) 
+grouped<-summarise(grouped, mean(freq), .groups = "keep")
+grouped <- grouped %>% rename(mean = `mean(freq)`)
+grouped$time_passed = as.numeric(grouped$time_passed)
+
+scale<-unname(quantile(grouped$time_passed))
+scale <- floor(scale)
+
+by_sec <- ggplot(grouped) + 
+  scale_x_time(breaks = scale) +
+  geom_line(aes(time_passed, mean, col=cpu_id)) + 
+  facet_wrap(~cpu_id) + 
+  xlab("Time") + 
+  ylab("Frequency") + 
+  theme(legend.position = "none")
+
+ggsave(
+  "benchmark_by_seconds.png",
+  plot = by_sec,
+  width = 10,
+  height = 5
+)
+
